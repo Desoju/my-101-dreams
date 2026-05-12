9cb772520aff85a1736729d20ebc741d8068d361
Julie Desova <desovajulie@seznam.cz>
2026-05-12 13:10:06 +0200

    added stats card for dreams to-be-completed

--- js/dreams/home/index-stats.js
+++ js/dreams/home/index-stats.js
@@ -1,12 +1,18 @@
 function updateHomeStats(dreams) {
   const completedDreamsCount = countDreamsByStatus(dreams, "dream_completed");
 
+  const visibleDreams = dreams.filter(function (dream) {
+    return dream.status !== "dream_not_attractive_anymore";
+  });
+
   updateStatCard(
     "globalDreamProgress",
-    `${completedDreamsCount} / ${dreams.length}`,
+    `${completedDreamsCount} / ${visibleDreams.length}`,
   );
 
   updateStatCard("activeDreams", countDreamsByStatus(dreams, "dream_active"));
+
+  updateStatCard("futureDreams", countDreamsByStatus(dreams, "dream_to_be_completed"));
 
   updateStatCard("priorityACount", getPriorityDreamCount(dreams, "A"));
 
